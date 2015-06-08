f661761fa5cb4daf48a156d54124b36851aa41fc
harunurhan <harunurhan17@gmail.com>
2015-06-08 17:07:40 +0300

    add specific color config support, add new comments to explain the code

--- config.js
+++ config.js
@@ -1,35 +1,71 @@
 var list = document.getElementById('pglist');
-var pages;
+/*
+This is used as hashmap where keys are page names 
+and values are colors selected for them
+*/
+var pages; 
 
-function loadTrustedPages() {
-  chrome.storage.sync.get({
-  	trustedPages: {} // no default
-  }, function(items) {
-  	pages = items.trustedPages;
-  	for (var page in pages) {
-  	  if (pages.hasOwnProperty(page)) { // just in case
-  	    addItemToList(page);
-  	  }
-    }
-  });
-}
-
-function addTrustedPage() {
-  var input = document.getElementById('pgin');
-  pages[input.value] = true; 
+/*
+This is used to save configuration updates and inserts to storage
+*/
+function savePageToStroge (pageName, color, callback) {
+  pages[pageName] = color; 
   chrome.storage.sync.set({
-  	trustedPages: pages // TODO: set only new page, not all of them everytime
-  }, function() {
-  	addItemToList(input.value);
-  });
+    trustedPages: pages // TODO: set only new page, not all of them everytime
+  }, callback);
 }
 
-function addItemToList(text) {
+var onColorInputChange = function (e) {
+  savePageToStroge(e.target.name, e.target.value);
+};
+
+/*
+This is used to modify config html when a new trusted webpage/hostname
+added.
+*/
+function addItemToHtmlList(pageName, color) {
   var item = document.createElement('li');
-  item.appendChild(document.createTextNode(text));
+  item.appendChild(document.createTextNode(pageName));
+  var colorSelector = document.createElement('input');
+  colorSelector.type = 'color';
+  colorSelector.value = color;
+  colorSelector.name = pageName; // will be used on event handling
+  colorSelector.addEventListener('input', onColorInputChange);
+  item.appendChild(colorSelector);
   list.appendChild(item);
 }
 
-document.addEventListener('DOMContentLoaded', loadTrustedPages);
+/*
+This is used to add new trusted webpage/hostname
+with specified color to be used for highlighting
+*/
+var onAddButtonClick = function() {
+  var pageInput = document.getElementById('pgin');
+  var pageName = pageInput.value;
+  var color = document.getElementById('colorin').value;
+  savePageToStroge(pageName, color, function() {
+    addItemToHtmlList(pageName, color);
+    pageInput.value = '';
+  });
+};
+
+/*
+This is used to add existing trusted webpages/hostnames
+from storage with their specified highlighting color
+*/
+var onContentLoad = function() {
+  chrome.storage.sync.get({
+    trustedPages: {} // no default
+  }, function(items) {
+    pages = items.trustedPages;
+    for (var pageName in pages) {
+      if (pages.hasOwnProperty(pageName)) { // just in case
+        addItemToHtmlList(pageName, pages[pageName]);
+      }
+    }
+  });
+};
+
+document.addEventListener('DOMContentLoaded', onContentLoad);
 document.getElementById('btnadd').addEventListener('click',
-    addTrustedPage);
+    onAddButtonClick);
--- highlight.js
+++ highlight.js
@@ -1,26 +1,31 @@
-var trustedPages;
-var highlightAction = function() {
-	var results = document.querySelectorAll('h3.r');
-	for(var i = 0; i < results.length; i++) {
-		var link = results[i].getElementsByTagName('a')[0];
-		if(trustedPages[link.hostname]) {// ~ O(1) complexity!
-			link.style.background = '#DFF0D8';
-		}
-	}
-};
+var trustedPages; // used as a hashmap where key is web page, value is boolean
 
-window.onload = function() {
-	var mo = new MutationObserver(highlightAction);
-	mo.observe(document.getElementById('res'),
-		  	   { childList: true, subtree: true });
-	console.log('The end of window.onload');
-};
-
-console.log('The end of highlight.js');
-
-// loading stored pages
 chrome.storage.sync.get({
   	trustedPages: {} // no default
   }, function(config) {
   	trustedPages = config.trustedPages;
 });
+
+var highlightAction = function() {
+	var results = document.querySelectorAll('h3.r');
+	for(var i = 0; i < results.length; i++) {
+		var link = results[i].getElementsByTagName('a')[0];
+		console.log('highlightAction: '+link.hostname); // DELETE
+		if(trustedPages.hasOwnProperty(link.hostname)) {// ~ O(1) complexity!
+			link.style.background = trustedPages[link.hostname]; // set defined color as background
+		}
+	}
+};
+
+window.onload = function() {
+	var resultsElement = document.getElementById('res');
+	var mutationObserver = new MutationObserver(highlightAction);
+	var observeOptions =  { childList: true, subtree: true };
+	if(resultsElement) { // returns null in some cases
+		mutationObserver.observe(resultsElement, observeOptions);
+	} else {
+		mutationObserver.observe(document.body, observeOptions); // TODO: find deeper item
+	}
+	console.log('The end of window.onload');
+};
+console.log('The end of highlight.js');
