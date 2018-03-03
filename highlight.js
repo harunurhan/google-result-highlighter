db81df1e03e55dca0bfd7eff53f9bb31a2a8dc19
harunurhan <harunurhan17@gmail.com>
2018-03-03 16:04:34 +0100

    close #5

--- highlight.js
+++ highlight.js
@@ -1,28 +1,21 @@
-var trustedPages; // used as a hashmap where key is web page, value is boolean
+var trustedPages; // used as a hashmap where key is web page, value is bg color
 
 chrome.storage.sync.get({
-  	trustedPages: {} // no default
-  }, function(config) {
-  	trustedPages = config.trustedPages;
+  	trustedPages: {}
+  }, function(storage) {
+  	trustedPages = storage.trustedPages;
 });
 
-var highlightAction = function() {
-	var results = document.querySelectorAll('h3.r');
+var highlightTrustedSources = function() {
+	var results = document.querySelectorAll('h3.r > a');
 	for (var i = 0; i < results.length; i++) {
-		var link = results[i].getElementsByTagName('a')[0];
+		var link = results[i];
 		if (trustedPages.hasOwnProperty(link.hostname)) { // ~ O(1) complexity!
 			link.style.background = trustedPages[link.hostname]; // set defined color as background
 		}
 	}
 };
 
-window.onload = function() {
-	var resultsElement = document.getElementById('res');
-	var mutationObserver = new MutationObserver(highlightAction);
-	var observeOptions =  { childList: true, subtree: true };
-	if (resultsElement) { //FIXME: returns null in some cases
-		mutationObserver.observe(resultsElement, observeOptions);
-	} else {
-		mutationObserver.observe(document.body, observeOptions); // TODO: find deeper item
-	}
-};
+window.addEventListener('load', function() {
+	highlightTrustedSources();
+});
